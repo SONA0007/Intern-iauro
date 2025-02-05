1200e444f08648f0ee5a9a5db38605a2856a79c9
sonal <sonalsanjaydiwan777@gmail.com>
2025-02-05 11:41:48 +0530

    add files

--- passengerCount/index.js
+++ passengerCount/index.js
@@ -1,9 +1,11 @@
 let count = 0;
+let save = document.getElementById('save');
 
 const display = document.getElementById('counter');
 const inCount = document.getElementById('increment');
 const deCount = document.getElementById('decrement');
 const reset = document.getElementById('reset');
+
 
 function updateDisplay(){
     display.textContent = count;
@@ -24,3 +26,10 @@ reset.addEventListener('click', () => {
     count = 0;
     updateDisplay();
 });
+
+function saveCount(){
+    let countStr = count + " - ";
+    save.innerText += countStr;
+    console.log(count);
+}
+
